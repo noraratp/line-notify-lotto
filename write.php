076c946f1adc45c12e3301402c4e78f8e7430e2a
Norarat.P <pnoraratp@gmail.com>
2019-01-22 22:20:28 +0700

    Update write.php

--- write.php
+++ write.php
@@ -1,7 +1,8 @@
 <html>
 <form>
-<div class="wrapper" style="display:none"></div>
+    <div class="wrapper" style="display:none"></div>
 </form>
+
 </html>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js'></script>
 <script>
@@ -28,39 +29,39 @@ request.onreadystatechange = function() {
     //$("#references").append(html);          
 };
 setTimeout(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const myParam = urlParams.get('round');
-        if (myParam != undefined) {
-            var round = "จับยี่กี รอบที่ " + myParam;
-            var three = $("th:contains(" + round + ")").next()[0].innerHTML;
-            var two = $("th:contains(" + round + ")").next().next()[0].innerHTML;
-            var array = [];
-            array.push(round);
-            array.push(three.trim());
-            array.push(two.trim());
-            var message = array;
-                // var postForm = { //Fetch form data
-                // 'value' : message //Store name fields value
-        };
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('round');
+    if (myParam != undefined) {
+        var round = "จับยี่กี รอบที่ " + myParam;
+        var three = $("th:contains(" + round + ")").next()[0].innerHTML;
+        var two = $("th:contains(" + round + ")").next().next()[0].innerHTML;
+        var array = [];
+        array.push(round);
+        array.push(three.trim());
+        array.push(two.trim());
+        var message = array;
+        // var postForm = { //Fetch form data
+        // 'value' : message //Store name fields value
+    };
 
-            //console.log(message)
-            if (three.trim().length == 3) {
-                console.log(JSON.stringify(postForm));
-                //window.location= "send.php?message="+message;
-                $.ajax({ //Process the form using $.ajax()
-                    type: 'POST', //Method type
-                    contentType: "application/json; charset=utf-8",
-                    url: '/line.php', //Your form processing file URL
-                    data: {"value" : message},//JSON.stringify(postForm),
-                    dataType: 'json',
-                    success: function(data) {
-                        console.log(data)
-                    }
-                });
-                
+    //console.log(message)
+    if (three.trim().length == 3) {
+        console.log(JSON.stringify(postForm));
+        //window.location= "send.php?message="+message;
+        $.ajax({ //Process the form using $.ajax()
+            type: 'POST', //Method type
+            contentType: "application/json; charset=utf-8",
+            url: '/line.php', //Your form processing file URL
+            data: {
+                "value": message
+            }, //JSON.stringify(postForm),
+            dataType: 'json',
+            success: function(data) {
+                console.log(data)
             }
-        } else {
-            window.location = "index.php?round=" + "- ไม่เจอรอบ -";
-        }
-    }, 10000);
+        });
+    } else {
+        window.location = "index.php?round=" + "- ไม่เจอรอบ -";
+    }
+}, 10000);
 </script>
