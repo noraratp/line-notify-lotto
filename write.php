c35939f4f0b5487968b15b5aa2ce85f0b070566f
Norarat.P <pnoraratp@gmail.com>
2019-01-22 21:32:25 +0700

    Update write.php

--- write.php
+++ write.php
@@ -48,7 +48,7 @@ setTimeout(() => {
                 //window.location= "send.php?message="+message;
                 $.ajax({ //Process the form using $.ajax()
                     type: 'POST', //Method type
-                    url: 'line.php', //Your form processing file URL
+                    url: '/line.php', //Your form processing file URL
                     data: JSON.stringify(postForm),
                     dataType: 'json',
                     success: function(data) {
