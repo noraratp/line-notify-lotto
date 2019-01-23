5e723a57a4c512db7439f139934a3f8d2c72b27f
Norarat.P <pnoraratp@gmail.com>
2019-01-23 14:01:36 +0700

    Update write.php

--- write.php
+++ write.php
@@ -19,49 +19,51 @@ function makeHttpObject() {
 
     throw new Error("Could not create HTTP request object.");
 }
-var request = makeHttpObject();
-request.open("GET", "https://www.jetsadabet.com/login", true);
-request.send(null);
-request.onreadystatechange = function() {
-    if (request.readyState == 4)
-        console.log(request.responseText);
-    $(".wrapper").html(request.responseText);
-    //$("#references").append(html);          
-};
-setTimeout(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const myParam = urlParams.get('round');
-    if (myParam != undefined) {
-        var round = "จับยี่กี รอบที่ " + myParam;
-        var three = $("th:contains(" + round + ")").next()[0].innerHTML;
-        var two = $("th:contains(" + round + ")").next().next()[0].innerHTML;
-        var array = [];
-        array.push(round);
-        array.push(three.trim());
-        array.push(two.trim());
-        var message = array;
-        // var postForm = { //Fetch form data
-        // 'value' : message //Store name fields value
+$(document).ready(function() {
+    var request = makeHttpObject();
+    request.open("GET", "https://www.jetsadabet.com/login", true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4)
+            console.log(request.responseText);
+        $(".wrapper").html(request.responseText);
+        //$("#references").append(html);          
     };
+    setTimeout(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('round');
+        if (myParam != undefined) {
+            var round = "จับยี่กี รอบที่ " + myParam;
+            var three = $("th:contains(" + round + ")").next()[0].innerHTML;
+            var two = $("th:contains(" + round + ")").next().next()[0].innerHTML;
+            var array = [];
+            array.push(round);
+            array.push(three.trim());
+            array.push(two.trim());
+            var message = array;
+            // var postForm = { //Fetch form data
+            // 'value' : message //Store name fields value
+        };
 
-    //console.log(message)
-    if (three.trim().length == 3) {
-        //console.log(JSON.stringify(postForm));
-        //window.location= "send.php?message="+message;
-        $.ajax({ //Process the form using $.ajax()
-            type: 'POST', //Method type
-            contentType: "application/json; charset=utf-8",
-            url: '/line.php', //Your form processing file URL
-            data: {
-                value: JSON.stringify(message)
-            }, //JSON.stringify(postForm),
-            dataType: 'json',
-            success: function(data) {
-                console.log(data)
-            }
-        });
-    } else {
-        window.location = "index.php?round=" + "- ไม่เจอรอบ -";
-    }
-}, 10000);
+        //console.log(message)
+        if (three.trim().length == 3) {
+            //console.log(JSON.stringify(postForm));
+            //window.location= "send.php?message="+message;
+            $.ajax({ //Process the form using $.ajax()
+                type: 'POST', //Method type
+                contentType: "application/json; charset=utf-8",
+                url: '/line.php', //Your form processing file URL
+                data: {
+                    value: JSON.stringify(message)
+                }, //JSON.stringify(postForm),
+                dataType: 'json',
+                success: function(data) {
+                    console.log(data)
+                }
+            });
+        } else {
+            window.location = "index.php?round=" + "- ไม่เจอรอบ -";
+        }
+    }, 10000);
+});
 </script>
